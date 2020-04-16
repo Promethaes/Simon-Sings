f937bb06bd213275732320a7d4e86d6c215f77b9
Anthony <anth19800@hotmail.com>
2020-04-15 20:24:37 -0400

    [Wip] game logic

--- include/SimonScene.h
+++ include/SimonScene.h
@@ -4,6 +4,21 @@
 #include "Cappuccino/SoundSystem.h"
 #include "Cappuccino/CappInput.h"
 using namespace Cappuccino;
+
+class ButtonSequence {
+public:
+	//1 is W, 2 is A, 3 is S, 4 is D
+	ButtonSequence(const std::vector<unsigned>& sequence);
+
+	void update(CappInput& _in);
+private:
+	unsigned lives = 3;
+	std::vector<unsigned> _sequence;
+	unsigned _iteration = 1;
+
+};
+
+
 class SimonScene : public Scene {
 public:
 	SimonScene(bool yn);
@@ -15,6 +30,8 @@ public:
 
 	void mouseFunction(double xpos, double ypos) override;
 private:
+	std::vector<ButtonSequence*> sequences;
+
 	FMOD_3D_ATTRIBUTES _attributes = { { 0 } };
 	FMOD_3D_ATTRIBUTES _attributes2 = { { 0 } };
 	CappInput _in;
--- src/SimonScene.cpp
+++ src/SimonScene.cpp
@@ -1,8 +1,10 @@
 #include "SimonScene.h"
+#include <iostream>
 
 SimonScene::SimonScene(bool yn)
 	:Scene(yn), _in(true, std::nullopt)
 {
+	sequences.push_back(new ButtonSequence({ 1,2,3,4 }));
 }
 
 void SimonScene::childUpdate(float dt)
@@ -10,8 +12,6 @@ void SimonScene::childUpdate(float dt)
 	FMOD_RESULT r;
 	// Position the listener at the origin
 
-	if (_in.keyboard->keyPressed(KeyEvent::W))
-		_attributes.position.x += 3.0f * dt;
 	_attributes.forward.z = -1.0f;
 	_attributes.up.y = 1.0f;
 	r = StudioSound::_system->setListenerAttributes(0, &_attributes);
@@ -21,6 +21,9 @@ void SimonScene::childUpdate(float dt)
 		Ghoul.playEvent(0);
 
 	Ghoul.getEvent(0)->set3DAttributes(&_attributes2);
+
+	sequences[0]->update(_in);
+
 }
 
 bool SimonScene::init()
@@ -36,7 +39,7 @@ bool SimonScene::init()
 	_attributes2.forward.z = -1.0f;
 	_attributes2.up.y = 1.0f;
 	FMOD_RESULT r;
-	
+
 	Ghoul.addEvent("event:/Ghoul/22 spotted");
 	Ghoul.getEvent(0)->set3DAttributes(&_attributes2);
 	Ghoul.playEvent(0);
@@ -58,3 +61,71 @@ bool SimonScene::exit()
 void SimonScene::mouseFunction(double xpos, double ypos)
 {
 }
+
+ButtonSequence::ButtonSequence(const std::vector<unsigned>& sequence)
+{
+	_sequence = sequence;
+}
+
+void ButtonSequence::update(CappInput& _in)
+{
+	if (!lives)
+		return;
+	std::vector<unsigned> subsequence;
+	subsequence.reserve(_iteration);
+
+	for (unsigned i = 0; i < _iteration; i++) {
+		subsequence.push_back(_sequence[i]);
+		std::cout << subsequence[i] << "\n";
+	}
+
+	std::vector<unsigned> inputsequence;
+	inputsequence.reserve(subsequence.size());
+
+	static bool wCon(0), aCon(0), sCon(0), dCon(0);
+	//W
+	if (_in.keyboard->keyPressed(KeyEvent::W) && !wCon) {
+		wCon = true;
+		inputsequence.push_back(1);
+	}
+	else if (_in.keyboard->keyReleased(KeyEvent::W) && wCon)
+		wCon = false;
+
+	//S
+	if (_in.keyboard->keyPressed(KeyEvent::S) && !sCon) {
+		sCon = true;
+		inputsequence.push_back(2);
+	}
+	else if (_in.keyboard->keyReleased(KeyEvent::S) && sCon)
+		sCon = false;
+
+	//A
+	if (_in.keyboard->keyPressed(KeyEvent::A) && !aCon) {
+		aCon = true;
+		inputsequence.push_back(3);
+	}
+	else if (_in.keyboard->keyReleased(KeyEvent::A) && aCon)
+		aCon = false;
+
+	//D
+	if (_in.keyboard->keyPressed(KeyEvent::D) && !dCon) {
+		dCon = true;
+		inputsequence.push_back(4);
+	}
+	else if (_in.keyboard->keyReleased(KeyEvent::D) && dCon)
+		dCon = false;
+
+	if (inputsequence == subsequence && inputsequence.size() == subsequence.size()) {
+		printf("next round!\n\n");
+		_iteration++;
+	}
+	else if (inputsequence != subsequence && inputsequence.size() == subsequence.size()) {
+		lives--;
+		if (!lives)
+			printf("you ran out of lives!\n");
+		printf("wrong! try again!\n");
+	}
+
+
+}
+
